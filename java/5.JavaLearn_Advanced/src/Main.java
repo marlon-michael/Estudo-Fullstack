de92e4ea349bb4560786aba84a471c57244ff364
Marlon Michael Pereira <marlonp@furb.br>
2022-06-15 19:56:37 -0300

    Entra21-2022

--- java/5.JavaLearn_Advanced/src/Main.java
+++ java/5.JavaLearn_Advanced/src/Main.java
@@ -10,7 +10,7 @@ public class Main {
         int exe = read.nextInt();
 
         if (exe == 1){
-            //read integer numbers until read num be zero
+            //add integer numbers into arraylist until read num be zero, then print array
             //ArrayList<Integer> array = new ArrayList<Integer>();
             ArrayList array = new ArrayList();
             while(true) {
@@ -24,7 +24,6 @@ public class Main {
             for (int i = 0; i < array.size()-1; i++) {
                 System.out.print(array.get(i) + " | ");
             }System.out.println(array.get(array.size()-1));
-
         }
 
     }
