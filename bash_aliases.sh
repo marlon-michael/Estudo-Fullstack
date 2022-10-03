a00c5ee481ba120f5a936fae84978bda06065f6f
Marlon Michael Pereira <marlonp@furb.br>
2022-10-03 18:27:53 -0300

    bash aliases

---
+++ bash_aliases.sh
@@ -0,0 +1,29 @@
+alias sts='git status';
+alias goclass='cd ~ && cd Documents/git/entra21-java-2022';
+alias gotcc='cd ~ && cd Documents/git/TCC-Entra21';
+
+
+function add(){
+  if [ "$1" == "" ]
+  then
+    git add . ;
+  elif [ "$1" == "restore" ]
+  then
+    git restore --staged . ;
+  else
+    git add "$1" ;
+  fi
+  git status;
+}
+
+function com() {
+  if [ "$1" = "" ]
+  then
+    echo default commit;
+    git commit -m "Entra21-2022";
+  else
+    echo commit: $1;
+    git commit -m "$1";
+  fi
+  git push;
+}
