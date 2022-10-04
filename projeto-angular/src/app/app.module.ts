c48aba1dde69cf7bc05d08623b192afebea079b9
Marlon Michael Pereira <marlonp@furb.br>
2022-10-03 21:37:52 -0300

    Angular project

--- projeto-angular/src/app/app.module.ts
+++ projeto-angular/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { HeaderComponent } from './header/header.component';
+import { HighlightCardComponent } from './highlight-card/highlight-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloWorldComponent,
+    HeaderComponent,
+    HighlightCardComponent
   ],
   imports: [
     BrowserModule
