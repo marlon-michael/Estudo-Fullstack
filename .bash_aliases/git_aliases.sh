b67bbc5ef72336ebd02d99df73f8dde5464e14c8
unknown <marlonmich7@gmail.com>
2023-07-12 11:37:03 -0300

    added block git push option to commit function

--- .bash_aliases/git_aliases.sh
+++ .bash_aliases/git_aliases.sh
@@ -8,7 +8,7 @@ alias push='git push';
 add() {
   if [ "$1" = "-r" ]
   then
-    echo -e '\n @ RUNNING: git restore --staged '"$2"'\n'; 
+    echo -e '\n[ @ RUNNING: git restore --staged '"$2"' ]\n'; 
     if [ "$2" = "." ] || [ "$2" = "" ]
     then
       git restore --staged . ;
@@ -17,37 +17,29 @@ add() {
     fi
   elif [ "$1" = "." ] || [ "$1" = "" ]
   then
-    echo -e '\n @ RUNNING: git add .\n';
+    echo -e '\n[ @ RUNNING: git add . ]\n';
     git add . ;
   else
-    echo -e '\n @ RUNNING: git add '"$1"'\n'; 
+    echo -e '\n[ @ RUNNING: git add '"$1" ]'\n'; 
     git add "$1" ;
   fi
   git status;
 }
 
-up() {
-  if [ "$1" = "" ]
+commit() {
+  if [ "$1" = "" ] || [ "$1" = "_" ]
   then
-    echo -e '\n @ RUNNING: git commit -m "nothing to report"\n';
+    echo -e '\n[ @ RUNNING: git commit -m "nothing to report" ]\n';
     git commit -m "default commit";
   else
-    echo -e '\n @ RUNNING: git commit -m: '"$1"'\n';
+    echo -e '\n[ @ RUNNING: git commit -m: '"$1"' ]\n';
     git commit -m "$1";
   fi
-  _push;
+  if [ "$2" != "-bp" ]
+  then
+    echo -e "\n[ @ RUNNING: git push ]\n";
+    git push;
+  fi
   echo -----------;
   git status;
-}
-
-_push(){
-  echo -----------;
-  echo "[ENTER] to git push your commit";
-  echo "[n] to skip git push";
-  read option;
-  if [ "$option" = "" ]
-  then
-    echo -e "\n @ RUNNING: git push\n"
-    push;
-  fi
 }
