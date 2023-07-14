29457ef130a3fef2af400b881b3f5f223dcafd5a
unknown <marlonmich7@gmail.com>
2023-07-14 15:36:18 -0300

    adicionados rotas de get all, get by id, post, patch by id, delete by id

--- rest api - mongodb nodejs/routes/routes.js
+++ rest api - mongodb nodejs/routes/routes.js
@@ -1,26 +1,64 @@
 const express = require('express')
+const Model = require('../model/model.js')
 
 
 const router = express.Router()
 
-router.post('/post', (req, res) => {
-    res.send('Post API')
+router.get('/get', async (req, res) => {
+    try{
+        const users = await Model.find()
+        res.status(200).json(users)
+    }
+    catch(error){
+        res.status(400).json({message: error.message})
+    }
 })
 
-router.get('/getAll', (req, res) => {
-    res.send('Get all API')
+router.get('/getById/:id', async (req, res) => {
+    try{
+        const user = await Model.findById(req.params.id)
+        res.status(200).json(user)
+    }
+    catch(error){
+        res.status(400).json({message: error.message})
+    }
 })
 
-router.get('/getById/:id', (req, res) => {
-    res.send('Get by id API')
+router.post('/post', async (req, res) => {
+    console.log(`$$$ ${res}`)
+    const user = new Model({ name: req.body.name })
+    try{
+        const savingUser = await user.save()
+        res.status(200).json(savingUser)
+    }
+    catch(error){
+        res.status(400).json({message: error.message})
+    }
 })
 
-router.patch('/post/:id', (req, res) => {
-    res.send('Update by id API')
+router.patch('/patch/:id', async (req, res) => {
+    try{
+        const filter = {_id: req.params.id}
+        const updatedUser = req.body
+        const options = {new: true}
+        const result = await Model.findOneAndUpdate(
+            filter, updatedUser, options
+        )
+        res.status(200).send(result)
+    }
+    catch(error){
+        res.status(400).json({message: error.message})
+    }
 })
 
-router.delete('/delete/:id', (req, res) => {
-    res.send('Delete by id API')
+router.delete('/delete/:id', async (req, res) => {
+    try{
+        const user = await Model.findByIdAndDelete(req.params.id)
+        res.status(200).send(`User [${user.name}] has been deleted`)
+    }
+    catch(error){
+        res.status(400).json({message: error.message})
+    }
 })
 
 module.exports = router
