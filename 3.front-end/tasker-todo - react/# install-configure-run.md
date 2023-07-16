26fca6778ddd39d8af5d4711c4086c5e027ceef9
unknown <marlonmich7@gmail.com>
2023-07-16 14:34:04 -0300

    lista de tarefas feita em React

---
+++ 3.front-end/tasker-todo - react/# install-configure-run.md
@@ -0,0 +1,14 @@
+# Como usar um aplicativo React
+
+### pré requisitos: 
+- node.js
+
+### Criando aplicativo react
+```console
+npx create-react-app name-of-project
+```
+
+### Rodando aplicação
+```console
+npm start
+```
---
+++ 3.front-end/tasker-todo - react/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import ToDo from './components/ToDo';
+
+
+function App() {
+  return (
+    <div className="app">
+      <div className="header"> <h3>Tasker</h3> </div>
+      <div className="body">
+        <ToDo></ToDo>
+      </div>
+    </div>
+  );
+}
+
+export default App;
