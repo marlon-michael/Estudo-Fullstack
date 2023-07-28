c37a3d297263a4abb2874ad776ccad7032deb8b1
unknown <marlonmich7@gmail.com>
2023-07-28 16:25:24 -0300

    pagina inicial

---
+++ 3.front-end/react-app/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+
+
+function Home(props: any) {
+	const session = props.props
+	
+	return (
+		<div className="body">
+			<h1>Hello {session.isLogged ? "Usuario" : "React"}</h1>
+		</div>
+	)
+}
+
+export default Home
