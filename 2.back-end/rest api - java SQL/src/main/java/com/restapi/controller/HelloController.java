10fb752bbac7b92ddb56481abb1f470b684e7079
unknown <marlonmich7@gmail.com>
2023-07-28 15:56:56 -0300

    Java, Springboot, SQL

--- 2.back-end/rest api - java SQL/src/main/java/com/restapi/controller/HelloController.java
+++ 2.back-end/rest api - java SQL/src/main/java/com/restapi/controller/HelloController.java
@@ -1,15 +1,15 @@
 package com.restapi.controller;
 
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 
 @RestController
 public class HelloController {
 
-    @GetMapping("/test")
-    public String index(){
-        return "Hello Spring";
+    @GetMapping("/")
+    public String index() {
+        return "Hello API powered by Spring boot";
     }
 
 }
