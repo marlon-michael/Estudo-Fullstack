70bf03517923747d602e10ff3abc565f598e0d2c
unknown <marlonmich7@gmail.com>
2023-08-06 11:38:24 -0300

    fundamentos basicos com typescript e angular

---
+++ 3.front-end/angular-typescript-fundamentos/src/app/heroes/heroes.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeroesScreenComponent } from './heroes-screen/heroes-screen.component';
+import { HeroListItemComponent } from './hero-list-item/hero-list-item.component';
+import { HeroesFormComponent } from './heroes-form/heroes-form.component';
+import { ReactiveFormsModule } from '@angular/forms'
+
+@NgModule({
+  declarations: [
+    HeroesScreenComponent,
+    HeroListItemComponent,
+    HeroesFormComponent
+  ],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ],
+  exports:[
+    HeroesScreenComponent,
+    HeroesFormComponent
+  ]
+})
+export class HeroesModule { }
