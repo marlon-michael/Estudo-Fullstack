3c4576887a49382a02fa8c786d12c232aea95655
unknown <marlonmich7@gmail.com>
2023-08-24 11:24:59 -0300

    adicionado funções basicas: adicionar contador, buscar sequencia contador

--- 2.back-end/rest-api_mongodb-nodejs/src/model/counterModel.js
+++ 2.back-end/rest-api_mongodb-nodejs/src/model/counterModel.js
@@ -2,8 +2,22 @@ const mongo = require('mongoose')
 
 
 const counterSchema = new mongo.Schema({
-    _id: {required: true, type: String},
-    sequence_value: {required: true, type: Number}
+    name: {required: true, type: String},
+    value: {required: true, type: Number}
 })
+const schema = mongo.model('counter', counterSchema)
 
-module.exports = mongo.model('counter', counterSchema)
+async function nextSequence(name){
+    var sequence = await schema.findOneAndUpdate({name}, {$inc:{value:1}}, {new: true})
+    return sequence.value
+}
+
+async function addCounter(name){
+    await schema.insertMany({name, value:0})
+}
+
+module.exports = {
+    schema,
+    nextSequence,
+    addCounter
+}
