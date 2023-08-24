1b787e19d040b61cca6c6ce5b199f2831df5a67f
unknown <marlonmich7@gmail.com>
2023-08-24 13:08:40 -0300

    adicionado rota de busca de cards

---
+++ 2.back-end/rest-api_mongodb-nodejs/src/routes/flashCardRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const flashCard = require('../model/flashCardModel')
+
+
+flashCardRouter = express.Router()
+
+flashCardRouter.get('/get', async (req, res) => {
+    try{
+        const search = await flashCard.find()
+        res.status(200).json(search)
+    }catch(error){
+        res.status(400).json({message: error.message})
+    }
+})
+
+
+module.exports = flashCardRouter
