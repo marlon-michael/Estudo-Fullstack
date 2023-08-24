31e7fa9c16afaecb03db6f653f1257acdd41b5d7
unknown <marlonmich7@gmail.com>
2023-08-24 16:43:10 -0300

    adicionado metodo POST para flash cards

--- 2.back-end/rest-api_mongodb-nodejs/src/routes/flashCardRoutes.js
+++ 2.back-end/rest-api_mongodb-nodejs/src/routes/flashCardRoutes.js
@@ -13,5 +13,15 @@ flashCardRouter.get('/get', async (req, res) => {
     }
 })
 
+flashCardRouter.post('/post', async (req, res) => {
+    try{
+        const card = flashCard(req.body)
+        result = card.save()
+        res.status(200).json(result)
+    }catch(error){
+        res.status(400).json({message: error.message})
+    }
+})
+
 
 module.exports = flashCardRouter
