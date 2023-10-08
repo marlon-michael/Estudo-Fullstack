68082d1759ec4143cf812a7b0aa557012631ec9a
unknown <marlonmich7@gmail.com>
2023-10-08 16:30:11 -0300

    menu principal

--- 4.mobile/MemorApp/App.js
+++ 4.mobile/MemorApp/App.js
@@ -1,29 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import Button from './component/Button';
 import SubTitle from './component/text/SubTitle';
 import Title from './component/text/Title';
+import Context from './hook/Context';
+
 
 export default function App() {
-  const [counter, setCounter] = useState(0)
+  const context = useContext(Context)
+  const [theme, setTheme] = useState(context.theme)
+
+  toggleTheme = (theme)=>{
+    if (theme === 'light') context.theme = 'light'
+    else if (theme === 'dark') context.theme = 'dark'
+    else if (context.theme === 'dark') context.theme = 'light'
+    else if (context.theme === 'light') context.theme = 'dark'
+    setTheme(x => x+1)
+  }
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: context.theme == 'dark'? '#223':'#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }
+  });
 
   return (
     <View style={styles.container}>
-      <Title>Hello world</Title>
-      <SubTitle>{counter}</SubTitle>
-      <Button onPress={()=>setCounter(old => old+1)}>increment</Button>
-      <StatusBar backgroundColor='#ddd'/>
+      <StatusBar backgroundColor={context.theme == 'dark'?'#112':"#eee"}/>
+      <Title> {context.theme} </Title>
+      <Button onPress={() => toggleTheme('light')}>light</Button>
+      <Button onPress={() => toggleTheme('dark')}>dark</Button>
+      <Button onPress={()=> toggleTheme()}>toggle</Button>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    fontSize: 20,
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
-});
