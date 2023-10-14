66484c538b5b4589df37c3756edca1e440d66974
unknown <marlonmich7@gmail.com>
2023-10-14 18:46:50 -0300

    contexto da aplicação

--- 4.mobile/MemorApp/hook/Context.js
+++ 4.mobile/MemorApp/hook/Context.js
@@ -1,9 +1,28 @@
-import { createContext, useState } from "react"
+import { createContext } from "react"
 
 
 
 const Context = createContext({
-    theme: 'dark'
+    darkmode: true,
+    tabHistory: [],
+
+    primaryDarkColor: '#334',
+    primaryLightColor: '#46b',
+
+    secondaryDarkColor: '#223',
+    secondaryLightColor: '#35a',
+
+    backgroundDarkColor: '#222229',
+    backgroundLightColor: '#fff',
+
+    secondaryBackgroundDarkColor: '#191920',
+    secondaryBackgroundLightColor: '#ddd',
+
+    statusBarDarkColor: '#16161b',
+    statusBarLightColor: '#ccc',
+
+    negativeDarkColor: '#eee',
+    negativeLightColor: '#2c2c3a'
 })
 
 
