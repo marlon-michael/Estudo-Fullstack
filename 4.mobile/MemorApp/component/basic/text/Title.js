31f0aa90296478543cf6a4bc5042190bfdf76ecf
unknown <marlonmich7@gmail.com>
2023-10-14 18:46:17 -0300

    componente de titulo

---
+++ 4.mobile/MemorApp/component/basic/text/Title.js
@@ -0,0 +1,27 @@
+import { useContext } from "react";
+import { StyleSheet, Text } from "react-native";
+import Context from "../../../hook/Context";
+
+export default function Title(props) {
+  const style = styles()
+
+  return (
+    <Text
+      style={[style.title, props.style]}
+      onPress={props.onPress}>{props.children}
+    </Text>
+  )
+}
+
+
+
+function styles() {
+  const app = useContext(Context)
+  return StyleSheet.create({
+    title: {
+      fontSize: 50,
+      color: app.darkmode? app.negativeDarkColor : app.negativeLightColor,
+      margin: 18
+    },
+  });
+}
