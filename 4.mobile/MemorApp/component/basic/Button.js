5caf90dac610f78607f599904b81e1ec45bd8066
unknown <marlonmich7@gmail.com>
2023-10-14 18:44:20 -0300

    componente de botao

---
+++ 4.mobile/MemorApp/component/basic/Button.js
@@ -0,0 +1,32 @@
+import { useContext } from "react";
+import { StyleSheet, Text } from "react-native";
+import Context from "../../hook/Context";
+
+
+
+export default function Button(props) {
+  const style = styles()
+  
+  return (
+    <Text
+      style={[style.button, props.style]}
+      onPress={props.onPress}
+    >{props.children}</Text>
+  )
+}
+
+
+
+function styles() {
+  const app = useContext(Context)
+  return StyleSheet.create({
+    button: {
+      fontSize: 20,
+      backgroundColor: app.darkmode ? app.primaryDarkColor : app.primaryLightColor,
+      color: 'whitesmoke',
+      borderRadius: 7,
+      padding: 10,
+      margin: 10
+    }
+  });
+}
