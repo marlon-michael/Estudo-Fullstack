e1d926ed9d7153192b58cde879c5a1ad0b879c5a
unknown <marlonmich7@gmail.com>
2024-03-13 22:55:30 -0600

    corrigido aumento desproporcional do campo Input

--- 4.mobile/ReactNative/MemorApp/component/root/Input.js
+++ 4.mobile/ReactNative/MemorApp/component/root/Input.js
@@ -1,18 +1,18 @@
 import { useContext } from "react"
 import { StyleSheet, TextInput } from "react-native"
-import themeContext from "../../hook/context/themeContext";
-
+import themeContext from "../../hook/context/themeContext"
 
 export default function Input(props) {
   const style = getStyle()
+  let height = (props.lines ?? 1) * 19
 
   return (
     <TextInput
-      style={style.input}
-      placeholderTextColor={'grey'}
-      numberOfLines={props.lines ?? 1}
-      multiline={props.lines ? true : false}
       {...props}
+      style={[style.input, { height: height + 25 }]}
+      placeholderTextColor={"grey"}
+      numberOfLines={props.lines}
+      multiline={props.lines > 1}
     />
   )
 }
@@ -26,30 +26,41 @@ function intToHex(int) {
 }
 
 function colorSum(color, sum) {
-  color = color.replace('#', '')
+  color = color.replace("#", "")
   let colorSplit = []
-  let colorChanging = '#'
-  if (color.length > 3) colorSplit = [color.substring(0, 2), color.substring(2, 4), color.substring(4, 6)]
-  if (color.length == 3) colorSplit = [color.substring(0, 1), color.substring(1, 2), color.substring(2, 3)]
-  colorSplit.forEach(hex => {
+  let colorChanging = "#"
+  if (color.length > 3)
+    colorSplit = [
+      color.substring(0, 2),
+      color.substring(2, 4),
+      color.substring(4, 6),
+    ]
+  if (color.length == 3)
+    colorSplit = [
+      color.substring(0, 1),
+      color.substring(1, 2),
+      color.substring(2, 3),
+    ]
+  colorSplit.forEach((hex) => {
     colorChanging += intToHex(hexToInt(hex) + sum)
   })
   return colorChanging
 }
 
-
 function getStyle() {
-  colorSum('#202025', - 1)
+  colorSum("#202025", -1)
   const theme = useContext(themeContext)
-  return (StyleSheet.create({
+  return StyleSheet.create({
     input: {
-      backgroundColor: theme.darkmode ? colorSum(theme.backgroundDarkColor, - 2) : colorSum(theme.backgroundColor, -1),
+      backgroundColor: theme.darkmode
+        ? colorSum(theme.backgroundDarkColor, -2)
+        : colorSum(theme.backgroundColor, -1),
       color: theme.darkmode ? theme.fontDarkColor : theme.fontColor,
       fontSize: 20,
       padding: 10,
       margin: 10,
       borderRadius: 10,
-      width: 400
-    }
-  }))
+      width: 400,
+    },
+  })
 }
