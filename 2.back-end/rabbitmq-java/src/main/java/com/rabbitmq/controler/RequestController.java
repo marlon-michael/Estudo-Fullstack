3705b302a7c491b61381e60ff03a8865ac1811fd
marlon-michael <marlonmich7@gmail.com>
2024-07-08 19:56:47 -0300

    adicionada rota para requisição por PathVariable utilizando metodo GET

--- 2.back-end/rabbitmq-java/src/main/java/com/rabbitmq/controler/RequestController.java
+++ 2.back-end/rabbitmq-java/src/main/java/com/rabbitmq/controler/RequestController.java
@@ -3,12 +3,14 @@ package com.rabbitmq.controler;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.rabbitmq.producer.Producer;
 
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.web.bind.annotation.PostMapping;
 
 @RequiredArgsConstructor
 @RestController
@@ -17,8 +19,13 @@ public class RequestController {
 
     private final Producer producer;
 
+    @GetMapping("/{message}")
+    public void getRequest(@PathVariable String message) {
+        producer.addToQueue(message);
+    }
+
     @PostMapping
-    public String Request(@RequestBody String entity) {
+    public String postRequest(@RequestBody String entity) {
         producer.addToQueue(entity);
         return entity;
     }
