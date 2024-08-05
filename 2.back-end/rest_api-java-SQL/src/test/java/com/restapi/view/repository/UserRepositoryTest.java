b464678ff04953053dbf6958bd092081a0887fee
unknown <marlonmich7@gmail.com>
2024-08-05 19:37:24 -0300

    teste unitarios tem suas modificacoes desfeitas apos execucao

--- 2.back-end/rest_api-java-SQL/src/test/java/com/restapi/view/repository/UserRepositoryTest.java
+++ 2.back-end/rest_api-java-SQL/src/test/java/com/restapi/view/repository/UserRepositoryTest.java
@@ -2,16 +2,21 @@ package com.restapi.view.repository;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
 import com.restapi.model.entity.User;
 import com.restapi.view.reposotiry.UserRepository;
 
 
 @DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+// @ActiveProfiles("test") // CONFIGURA A APLICAÇÃO PARA FUNCIONAR COM "application-test.properties"
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE) // CONFIGURA A APLICAÇÃO PARA FUNCIONAR COM "application.properties"
 public class UserRepositoryTest {
 
   @Autowired
@@ -28,10 +33,12 @@ public class UserRepositoryTest {
     
     // BUSCANDO USUARIO
     List<User> user = userRepository.findByName(username);
+    System.out.println(user);
 
     // VERIFICANDO SE VALORES SÃO OS ESPERADOS
     assertEquals(username, user.get(0).getName());
     assertEquals(1, user.size());
+    userRepository.delete(user.get(0));
   }
 
 }
