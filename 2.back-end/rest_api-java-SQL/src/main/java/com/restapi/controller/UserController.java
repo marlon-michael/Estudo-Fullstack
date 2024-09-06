7b389c173058a99230f590044113c434c7665a07
marlon-michael <marlonmich7@gmail.com>
2024-09-06 19:49:13 -0300

    userController agora utiliza userService

--- 2.back-end/rest_api-java-SQL/src/main/java/com/restapi/controller/UserController.java
+++ 2.back-end/rest_api-java-SQL/src/main/java/com/restapi/controller/UserController.java
@@ -1,7 +1,8 @@
 package com.restapi.controller;
 
 import com.restapi.model.entity.User;
-import com.restapi.view.reposotiry.UserRepository;
+import com.restapi.view.service.UserService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,26 +14,23 @@ import java.util.List;
 public class UserController {
 
     @Autowired
-    private UserRepository userRepository;
+    private UserService userService;
 
     @GetMapping("/get")
     public List<User> findAll(){
-        return userRepository.findAll();
+        return userService.findAll();
     }
 
-    @GetMapping("/get/{id}")
-    public User findByName(@PathVariable Long id){
-        if (userRepository.findById(id).isPresent()){
-            return userRepository.findById(id).get();
-        }
-        else return null;
+    @GetMapping("/get/{name}")
+    public User findByName(@PathVariable String name){
+        return userService.findByName(name).get(0);
     }
 
     @PostMapping("/post")
     public User saveUser(@RequestBody User newUser){
         try{
             newUser.setName(newUser.getName().replace("@", " "));
-            userRepository.save(newUser);
+            userService.save(newUser);
             return newUser;
         }
         catch(Exception error){
@@ -44,37 +42,12 @@ public class UserController {
 
     @PutMapping("/patch/{id}")
     public User updateUser(@PathVariable Long id, @RequestBody User updatedUser){
-        try{
-            User user = new User();
-            if (userRepository.findById(id).isPresent()) {
-                user = userRepository.findById(id).get();
-                user.setName(updatedUser.getName().replace("@", " "));
-                userRepository.save(user);
-                return user;
-            }
-            else return null;
-        }
-        catch (Exception error){
-            System.out.println(error.getMessage());
-            return null;
-        }
+        return userService.updatedUser(id, updatedUser);
     }
 
     @DeleteMapping("/delete/{id}")
     public User deleteUser(@PathVariable Long id){
-        try{
-            User user = new User();
-            if (userRepository.findById(id).isPresent()){
-                user = userRepository.findById(id).get();
-                userRepository.deleteById(id);
-                return user;
-            }
-            else return null;
-        }
-        catch(Exception error){
-            System.out.println(error.getMessage());
-            return null;
-        }
+        return userService.deleteUser(id);
     }
 
 }
