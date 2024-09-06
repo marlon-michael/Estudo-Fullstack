b2e7025a0e914fb48c5629d198cfae4803f41bd1
marlon-michael <marlonmich7@gmail.com>
2024-09-06 19:51:45 -0300

    metodos mais isolados, interferindo menos entre si

--- 2.back-end/rest_api-java-SQL/src/test/java/com/restapi/view/repository/UserRepositoryTest.java
+++ 2.back-end/rest_api-java-SQL/src/test/java/com/restapi/view/repository/UserRepositoryTest.java
@@ -3,12 +3,10 @@ package com.restapi.view.repository;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.util.List;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.ActiveProfiles;
 
 import com.restapi.model.entity.User;
 import com.restapi.view.reposotiry.UserRepository;
