4a1d3881e9e4bc215ff10a53478398073bdc3f00
marlon-michael <marlonmich7@gmail.com>
2024-10-09 19:50:55 -0300

    tipagem de configuração CORS; suporte ao OpenFeign

--- 2.back-end/rest_api-java-SQL/src/main/java/com/restapi/RestApiApplication.java
+++ 2.back-end/rest_api-java-SQL/src/main/java/com/restapi/RestApiApplication.java
@@ -3,23 +3,27 @@ package com.restapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import jakarta.servlet.Filter;
+
 import java.util.Arrays;
 import java.util.Collections;
 
+@EnableFeignClients
 @SpringBootApplication
 public class RestApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestApiApplication.class, args);
 	}
-
+	
 	@Bean
-	public FilterRegistrationBean corsFilter(){
+	public FilterRegistrationBean<Filter> corsFilter(){
 		final UrlBasedCorsConfigurationSource src = new UrlBasedCorsConfigurationSource();
 		final CorsConfiguration config = new CorsConfiguration();
 		config.setAllowCredentials(true);
@@ -27,7 +31,7 @@ public class RestApiApplication {
 		config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept"));
 		config.setAllowedMethods(Arrays.asList("GET", "POST", "UPDATE", "DELETE", "OPTIONS", "PATCH"));
 		src.registerCorsConfiguration("/**", config);
-		FilterRegistrationBean registration = new FilterRegistrationBean(new CorsFilter(src));
+		FilterRegistrationBean<Filter> registration = new FilterRegistrationBean<Filter>(new CorsFilter(src));
 		return registration;
 	}
 
