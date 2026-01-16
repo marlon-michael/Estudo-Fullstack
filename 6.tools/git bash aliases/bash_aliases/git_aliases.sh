5f965104f94f94991f4b1855b79e393169c7d252
Marlon Michael <marlonmich7@gmail.com>
2026-01-16 12:57:19 -0300

    aprimorada alias para git commit e git push

--- 6.tools/git bash aliases/bash_aliases/git_aliases.sh
+++ 6.tools/git bash aliases/bash_aliases/git_aliases.sh
@@ -12,7 +12,7 @@ ga() {
 		echo -e "
 ga \$1 \$2
 		
-\$1: 
+- \$1: 
 	* -r \$2		: remove file \$2 ( git restore --staged \"\$2\")
 	* \".\" or \"\"	: add all new/updated files
 	* \"file\"	: add specified file
@@ -47,27 +47,51 @@ gc() {
 		echo -e "
 gc \$1 \$2
 		
-\$1: 
 	* \"_\" or \"\"	: commit as \"nothing to report\"
 	* \"message\"	: add specified message
-	
-\$2: 
-	* -bp		: dont exec git push at the end
+	* -p		: exec git push at the end
 		"
 		return;
 	fi
-	if [ "$1" = "" ] || [ "$1" = "_" ]
-	then
+	
+	doPush(){
+		echo -e "\n>>> Running:[ git push ]\n";
+		command git push;
+	}
+	
+	doEmptyCommit(){
 		echo -e '\n>>> Running:[ git commit -m "nothing to report" ]\n';
 		git commit -m "default commit";
-	else
-		echo -e '\n>>> Running:[ git commit -m: '"$1"' ]\n';
-		git commit -m "$1";
-	fi
-	if [ "$2" != "-bp" ]
-	then
-		echo -e "\n>>> Running:[ git push ]\n";
-		git push;
-	fi
+	}
+	
+	doCommit(){
+		echo -e '\n>>> Running:[ git commit -m '"$gitAliases_messageToCommit"' ]\n';
+		git commit -m "$gitAliases_messageToCommit"
+	}
+	
+	local gitAliases_messageToCommit=""
+	local initCommand=()
+	local endingCommand=()
+	for arg in "$@"; do
+        if [[ "$arg" = *"-"* ]]
+        then
+        	if [[ "$arg" = "-p" ]]
+        	then
+        		endingCommand+=("doPush");
+        	fi
+        else
+        	if [ "$arg" = "" ] || [ "$arg" = "_" ]
+        	then
+				initCommand+=("doEmptyCommit");
+			else
+				gitAliases_messageToCommit="$arg"
+				initCommand+=("doCommit");
+			fi
+        fi
+    done
+    
+	for cmd in "${initCommand[@]}"; do "$cmd"; done
+    for cmd in "${endingCommand[@]}"; do "$cmd"; done
+	
 	git status;
 }
